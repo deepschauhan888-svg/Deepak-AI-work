24c6eed8150da5194f85bd8673419bc00d0d2fc8
deepschauhan888-svg <deeps.chauhan.888@gmail.com>
2026-08-12 17:02:02 +0530

    Update workbench.ts

--- lib/workbench.ts
+++ lib/workbench.ts
@@ -2,6 +2,9 @@ export type WorkbenchItem = {
   n: string;
   title: string;
   blurb: string;
+  tagline?: string;
+  aiHelpsWith?: string;
+  humanLed?: string;
 };
 
 export const WORKBENCH: WorkbenchItem[] = [
